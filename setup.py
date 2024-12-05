d4e6fb5aca81f50b8cc291f1f0e8283607d59fee
gbolly <okerayigbolahan@gmail.com>
2024-12-05 14:04:45 +0100

    MAINT fix package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("package.json") as f:
     package = json.load(f)
 long_description = (here / "README.md").read_text()
 
-package_name = "dash-chat"
+package_name = package["name"].replace(" ", "_").replace("-", "_")
 
 setup(
     name=package_name,
