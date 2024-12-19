a8c49c393330c5048b0e0eee49f78397507cd34c
gbolly <okerayigbolahan@gmail.com>
2024-12-19 23:13:50 +0100

    remove private components to a separate folder outside lib dir

--- dash_chat/_imports_.py
+++ dash_chat/_imports_.py
@@ -1,11 +1,5 @@
 from .ChatComponent import ChatComponent
-from .ChatMessageInput import ChatMessageInput
-from .DotsIndicator import DotsIndicator
-from .SpinnerIndicator import SpinnerIndicator
 
 __all__ = [
-    "ChatComponent",
-    "ChatMessageInput",
-    "DotsIndicator",
-    "SpinnerIndicator"
+    "ChatComponent"
 ]
